e274f3149677249e39d2cade024f7dec8addc1bf
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-22 14:01:51 -0600

    Passed all tests

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
-
+  function fizzy(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      return "FizzBuzz";
+    }
+    else if(num % 3 === 0) {
+      return "Fizz";
+    } else if(num % 5 === 0) {
+      return "Buzz";
+    } else {
+      return num;
+    }
+  }
 
 
 
